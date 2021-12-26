326f5ef320e542f51df32b6fed8e426eba84e24b
Trung nq <86708180+Cucasu0209@users.noreply.github.com>
2021-12-26 22:05:46 +0700

    Delete Src/Week3 directory
    fdsfsdf

--- Src/Week3/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module com.example.aimsproject {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-
-    opens com.example.aimsproject to javafx.fxml;
-    exports com.example.aimsproject;
-}
